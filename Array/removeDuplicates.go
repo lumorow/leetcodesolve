893b5944c4369ce4a8e78e930f2cbe04a5873b36
Luka Zhovtovsky <l.zhovtovskiy@2gis.ru>
2024-11-03 22:12:58 +0700

    Best Time to Buy and Sell Stock II

--- Array/removeDuplicates.go
+++ Array/removeDuplicates.go
@@ -1,13 +1,20 @@
 package main
 
-import "fmt"
-
 // Remove Duplicates from Sorted Array
 // https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
+
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once.
+// The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially.
+// The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+
 // Input: nums = [0,0,1,1,1,2,2,3,3,4]
 // Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
 // Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
 // It does not matter what you leave beyond the returned k (hence they are underscores).
+
 func removeDuplicates(nums []int) int {
 	exists := make(map[int]struct{}, len(nums)/2)
 	count := 0
@@ -24,10 +31,4 @@ func removeDuplicates(nums []int) int {
 	}
 
 	return count
-}
-
-func main() {
-	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
-	removeDuplicates(nums)
-	fmt.Println(nums)
 }
